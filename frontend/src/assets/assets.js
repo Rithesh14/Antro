906a5303a863281c4ff202b47b07ed7b420606c3
Rithesh14 <167207142+Rithesh14@users.noreply.github.com>
2025-05-12 18:08:29 +0530

    Add files via upload

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,408 @@
+import home_img from './home_img.jpg';
+import Antro_logo from './Antro_logo.png';
+import Antro_transparent from './Antro_transparent.png';
+import choose_material from './choose_material.jpg';
+import L1_1 from './L1_1.jpg';
+import B1_2 from './B1_2.jpg';
+import B1_1 from './B1_1.jpg';
+import L1_2 from './L1_2.jpg';
+import L1_3 from './L1_3.jpg';
+import O1_1 from './O1_1.jpg';
+import cross_icon from './cross_icon.jpg';
+import search from './search.png';
+import shopping_cart from './shopping_cart.png';
+import user from './user.png';
+import ai_gen from './ai_gen.jpg';
+import Bedroom from './Bedroom.jpg';
+import Diningroom from './Diningroom.jpg';
+import office from './office.jpg';
+import Livingroom from './Livingroom.jpg';
+import L1_4_1 from './L1_4_1.jpg';
+import L1_4_2 from './L1_4_2.jpg';
+import L1_4_3 from './L1_4_3.jpg';
+import L1_5_1 from './L1_5_1.jpg';
+import L1_5_2 from './L1_5_2.jpg';
+import L1_5_3 from './L1_5_3.jpg';
+import L1_6_1 from './L1_6_1.jpg';
+import L1_6_2 from './L1_6_2.jpg';
+import L1_6_3 from './L1_6_3.jpg';
+import B1_3_1 from './B1_3_1.jpg';
+import B1_3_2 from './B1_3_2.jpg';
+import B1_3_3 from './B1_3_3.jpg';
+import B1_4_1 from './B1_4_1.jpg';
+import B1_4_2 from './B1_4_2.jpg';
+import B1_4_3 from './B1_4_3.jpg';
+import B1_5_1 from './B1_5_1.jpg';
+import B1_5_2 from './B1_5_2.jpg';
+import B1_5_3 from './B1_5_3.jpg';
+import B1_6_1 from './B1_6_1.jpg';
+import B1_6_2 from './B1_6_2.jpg';
+import B1_6_3 from './B1_6_3.jpg';
+import O1_2_1 from './O1_2_1.jpg';
+import O1_2_2 from './O1_2_2.jpg';
+import O1_2_3 from './O1_2_3.jpg';
+import O1_3_1 from './O1_3_1.jpg';
+import O1_3_2 from './O1_3_2.jpg';
+import O1_3_3 from './O1_3_3.jpg';
+import O1_4_1 from './O1_4_1.jpg';
+import O1_4_2 from './O1_4_2.jpg';
+import O1_4_3 from './O1_4_3.jpg';
+import O1_5_1 from './O1_5_1.jpg';
+import O1_5_2 from './O1_5_2.jpg';
+import O1_5_3 from './O1_5_3.jpg';
+import O1_6_1 from './O1_6_1.jpg';
+import O1_6_2 from './O1_6_2.jpg';
+import O1_6_3 from './O1_6_3.jpg';
+import K1_1_1 from './K1_1_1.jpg';
+import K1_1_2 from './K1_1_2.jpg';
+import K1_1_3 from './K1_1_3.jpg';
+import K1_2_1 from './K1_2_1.jpg';
+import K1_2_2 from './K1_2_2.jpg';
+import K1_2_3 from './K1_2_3.jpg';
+import K1_3_1 from './K1_3_1.jpg';
+import K1_3_2 from './K1_3_2.jpg';
+import K1_3_3 from './K1_3_3.jpg';
+import K1_4_1 from './K1_4_1.jpg';
+import K1_4_2 from './K1_4_2.jpg';
+import K1_4_3 from './K1_4_3.jpg';
+import K1_5_1 from './K1_5_1.jpg';
+import K1_5_2 from './K1_5_2.jpg';
+import K1_5_3 from './K1_5_3.jpg';
+import K1_6_1 from './K1_6_1.jpg';
+import K1_6_2 from './K1_6_2.jpg';
+import K1_6_3 from './K1_6_3.jpg';
+import OurPage from './OurPage.jpg';
+import about_img from './about_img.jpg';
+import razorpay from './razorpay.png';
+import gpay from './gpay.png';
+import mastercard from './mastercard.png';
+import stripe from './stripe.png';
+import cash from './cash.png';
+import Glogo from './Glogo.png';
+
+export const assets = {
+  home_img,
+  Antro_logo,
+  Antro_transparent,
+  choose_material,
+  cross_icon,
+  stripe,
+  cash,
+  L1_1,
+  B1_2,
+  B1_1,
+  L1_2,
+  L1_3,
+  O1_1,
+  search,
+  shopping_cart,
+  user,
+  ai_gen,
+  Bedroom,
+  Diningroom,
+  office,
+  Livingroom,
+  L1_4_1,
+  L1_4_2,
+  L1_4_3,
+  L1_5_1,
+  L1_5_2,
+  L1_5_3,
+  L1_6_1,
+  L1_6_2,
+  L1_6_3,
+  B1_3_1,
+  B1_3_2,
+  B1_3_3,
+  B1_4_1,
+  B1_4_2,
+  B1_4_3,
+  B1_5_1,
+  B1_5_2,
+  B1_5_3,
+  B1_6_1,
+  B1_6_2,
+  B1_6_3,
+  O1_2_1,
+  O1_2_2,
+  O1_2_3,
+  O1_3_1,
+  O1_3_2,
+  O1_3_3,
+  O1_4_1,
+  O1_4_2,
+  O1_4_3,
+  O1_5_1,
+  O1_5_2,
+  O1_5_3,
+  O1_6_1,
+  O1_6_2,
+  O1_6_3,
+  K1_1_1,
+  K1_1_2,
+  K1_1_3,
+  K1_2_1,
+  K1_2_2,
+  K1_2_3,
+  K1_3_1,
+  K1_3_2,
+  K1_3_3,
+  K1_4_1,
+  K1_4_2,
+  K1_4_3,
+  K1_5_1,
+  K1_5_2,
+  K1_5_3,
+  K1_6_1,
+  K1_6_2,
+  K1_6_3,
+  OurPage,
+  about_img,
+  mastercard,
+  gpay,
+  razorpay,
+  Glogo,
+};
+
+export const products = [
+  {
+    _id: "L1_1",
+    name: "Rounded Sofa",
+    description: "Comfy sofa for all members of the family",
+    price: 300,
+    image: [L1_1],
+    bestseller: true,
+    category: "Living Room",
+    date: "2023-03-15"
+  },
+  {
+    _id: "B1_1",
+    name: "Cushioned Chair",
+    description: "Fluffy sofa cum chair for more comfort",
+    price: 200,
+    image: [B1_1],
+    bestseller: true,
+    category: "Bedroom",
+    date: "2022-08-11"
+  },
+  {
+    _id: "B1_2",
+    name: "Cushioned Chair with Support",
+    description: "Fluffy sofa cum chair but with extra support for relaxing",
+    price: 250,
+    image: [B1_2],
+    bestseller: true,
+    category: "Bedroom",
+    date: "2024-01-29"
+  },
+  {
+    _id: "L1_2",
+    name: "Modular Sofa",
+    description: "Sofa that fits every aspect of the room",
+    price: 210,
+    image: [L1_2],
+    bestseller: true,
+    category: "Living Room",
+    date: "2021-12-05"
+  },
+  {
+    _id: "L1_3",
+    name: "Leathered Sofa",
+    description: "Sofa that enhances the room's luxury",
+    price: 270,
+    image: [L1_3],
+    bestseller: true,
+    category: "Living Room",
+    date: "2023-06-20"
+  },
+  {
+    _id: "O1_1",
+    name: "Cushioned Chair with Support (Office Edition)",
+    description: "With both the features of comfort and looks, it's the best in the segment",
+    price: 600,
+    image: [O1_1],
+    bestseller: true,
+    category: "Office",
+    date: "2022-11-03"
+  },
+  {
+    _id: "L1_4",
+    name: "White Polished Cabinet",
+    description: "A simple work of art, just white",
+    price: 700,
+    image: [L1_4_1, L1_4_2, L1_4_3],
+    bestseller: true,
+    category: "Living Room",
+    date: "2022-01-21"
+  },
+  {
+    _id: "L1_5",
+    name: "Brown Polished Cabinet",
+    description: "Natural color cabinet for a woody feeling in the house",
+    price: 750,
+    image: [L1_5_1, L1_5_2, L1_5_3],
+    bestseller: true,
+    category: "Living Room",
+    date: "2024-08-11"
+  },
+  {
+    _id: "L1_6",
+    name: "Conjoined Blue Sofa",
+    description: "Large sofa for the sitting of many people, whether family, guests, or relatives",
+    price: 700,
+    image: [L1_6_1, L1_6_2, L1_6_3],
+    bestseller: true,
+    category: "Living Room",
+    date: "2022-01-21"
+  },
+  {
+    _id: "B1_3",
+    name: "Brown Furnished Cupboard",
+    description: "A simple natural-looking cupboard for a simple choice",
+    price: 1200,
+    image: [B1_3_1, B1_3_2, B1_3_3],
+    bestseller: true,
+    category: "Bedroom",
+    date: "2021-08-18"
+  },
+  {
+    _id: "B1_4",
+    name: "White Modular Cupboard",
+    description: "White stylish modular cupboard for a luxury look",
+    price: 1300,
+    image: [B1_4_1, B1_4_2, B1_4_3],
+    bestseller: true,
+    category: "Bedroom",
+    date: "2024-10-29"
+  },
+  {
+    _id: "B1_5",
+    name: "Queen Sized Bed",
+    description: "A bed with large space and simplicity",
+    price: 1600,
+    image: [B1_5_1, B1_5_2, B1_5_3],
+    bestseller: true,
+    category: "Bedroom",
+    date: "2020-02-20"
+  },
+  {
+    _id: "B1_6",
+    name: "Small Sized Bed",
+    description: "A little bed for a single person but with extra comfort",
+    price: 1000,
+    image: [B1_6_1, B1_6_2, B1_6_3],
+    bestseller: true,
+    category: "Bedroom",
+    date: "2019-01-04"
+  },
+  {
+    _id: "O1_2",
+    name: "Ergonomic Office Chair",
+    description: "Chair with stable posture and can work with long hours",
+    price: 400,
+    image: [O1_2_1, O1_2_2, O1_2_3],
+    bestseller: true,
+    category: "Office",
+    date: "2023-01-03"
+  },
+  {
+    _id: "O1_3",
+    name: "Office Documents Cabinet",
+    description: "Cabinet with large storage space can fit many files together",
+    price: 900,
+    image: [O1_3_1, O1_3_2, O1_3_3],
+    bestseller: true,
+    category: "Office",
+    date: "2021-05-30"
+  },
+  {
+    _id: "O1_4",
+    name: "Comfortable Cushioned Leathered Office Chair",
+    description: "Can be considered in luxury and comfort simultaneously",
+    price: 700,
+    image: [O1_4_1, O1_4_2, O1_4_3],
+    bestseller: true,
+    category: "Office",
+    date: "2024-07-10"
+  },
+  {
+    _id: "O1_5",
+    name: "Office Desk Set",
+    description: "Simple office desk set for the base of the employees",
+    price: 1200,
+    image: [O1_5_1, O1_5_2, O1_5_3],
+    bestseller: true,
+    category: "Office",
+    date: "2022-06-17"
+  },
+  {
+    _id: "O1_6",
+    name: "Mini Sized Office Chair",
+    description: "Mini-sized black chair with cost efficiency",
+    price: 500,
+    image: [O1_6_1, O1_6_2, O1_6_3],
+    bestseller: true,
+    category: "Office",
+    date: "2021-11-07"
+  },
+  {
+    _id: "K1_1",
+    name: "Brown Polished Kitchen Cabinet",
+    description: "Kitchen cabinet for the storage of essentials",
+    price: 700,
+    image: [K1_1_1, K1_1_2, K1_1_3],
+    bestseller: true,
+    category: "Dining Room",
+    date: "2020-09-18"
+  },
+  {
+    _id: "K1_2",
+    name: "Foldable Dining Set",
+    description: "With low room space and enough creativity, this dining set is a masterclass",
+    price: 1100,
+    image: [K1_2_1, K1_2_2, K1_2_3],
+    bestseller: true,
+    category: "Dining Room",
+    date: "2023-05-11"
+  },
+  {
+    _id: "K1_3",
+    name: "Mini Dining Set",
+    description: "For one or a couple, this dining set is just perfect",
+    price: 850,
+    image: [K1_3_1, K1_3_2, K1_3_3],
+    bestseller: true,
+    category: "Dining Room",
+    date: "2024-02-08"
+  },
+  {
+    _id: "K1_4",
+    name: "Decorative Dining Chair",
+    description: "Chair designed with artistry, just marvelous",
+    price: 500,
+    image: [K1_4_1, K1_4_2, K1_4_3],
+    bestseller: true,
+    category: "Dining Room",
+    date: "2022-09-11"
+  },
+  {
+    _id: "K1_5",
+    name: "Six Seater Dining Set",
+    description: "Dining for joint families or guests, it's accommodating",
+    price: 1100,
+    image: [K1_5_1, K1_5_2, K1_5_3],
+    bestseller: true,
+    category: "Dining Room",
+    date: "2020-03-09"
+  },
+  {
+    _id: "K1_6",
+    name: "Cushioned Dining Chair",
+    description: "Dining chair for comfort and dining both",
+    price: 500,
+    image: [K1_6_1, K1_6_2, K1_6_3],
+    bestseller: true,
+    category: "Dining Room",
+    date: "2022-12-11"
+  },
+];
