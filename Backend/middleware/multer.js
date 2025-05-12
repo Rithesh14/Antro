f6f3496275872b98b8ee91e74e5c394efbb31128
Rithesh14 <167207142+Rithesh14@users.noreply.github.com>
2025-05-12 17:59:38 +0530

    Add files via upload

---
+++ Backend/middleware/multer.js
@@ -0,0 +1,23 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file size limit
+  fileFilter: (req, file, callback) => {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    if (mimetype && extname) {
+      return callback(null, true);
+    }
+    callback(new Error('File type not supported'));
+  }
+});
+
+const upload = multer({ storage });
+
+export default upload;
